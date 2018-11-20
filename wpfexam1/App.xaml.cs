00e056f75226910041d0b46fa86f125cb3f5dd57
JunHwan Oh <ogunhan@naver.com>
2018-11-20 09:32:25 +0900

    Startup으로 설정

--- wpfexam1/App.xaml.cs
+++ wpfexam1/App.xaml.cs
@@ -13,5 +13,16 @@ namespace wpfexam1
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)         // StartUp 에는 대리자가 필요하므로 좌측 문장은 항상 필요하다.
+        {
+            // 메인 윈도우를 초기화하고 보여준다.( Initialize and Show the mainWindow )
+            var mainWindow = new MainWindow();
+            this.MainWindow = mainWindow;
+            mainWindow.Show();
+
+            // mainWindow 의 DataContext 에 ViewModel 을 연결한다. (Connect the ViewModel to DataContext of mainWindow )
+            var viewModel = new ViewModel();
+            mainWindow.DataContext = viewModel;
+        }
     }
 }
--- wpfexam1/MainWindow.xaml.cs
+++ wpfexam1/MainWindow.xaml.cs
@@ -23,7 +23,6 @@ namespace wpfexam1
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new ViewModel();
         }
     }
 }
